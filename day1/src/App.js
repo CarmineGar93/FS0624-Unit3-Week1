b18c2c36e7a118ec6748034c051ea576acf5103d
Carmy93 <carmygargiulo45@gmail.com>
2024-08-19 14:43:58 +0200

    up

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import ButtonComponent from './components/ButtonComponent';
 import ImageComponent from './components/ImageComponent';
-
+const nomi = ['Salva', 'Reset', 'Cancella']
+const scelto = nomi[Math.floor(Math.random() * 3)]
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Bottone dinamico</h1>
-        <ButtonComponent text="Ciao" />
+        <ButtonComponent text={scelto} />
         <h1>Immagine dinamica</h1>
         <ImageComponent src="https://placedog.net/300" alt="Doggo" css="App-logo" />
       </header>
