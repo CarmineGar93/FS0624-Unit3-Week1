a0ee9c0f92ecd17dc24c8ca9e3bd07555d8429c0
Carmy93 <carmygargiulo45@gmail.com>
2024-08-19 15:36:41 +0200

    up

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import ButtonComponent from './components/ButtonComponent';
 import ImageComponent from './components/ImageComponent';
+import ButtonComponentDynamic from './components/ButtonComponentDynamic';
 const nomi = ['Salva', 'Reset', 'Cancella']
 let scelto = nomi[Math.floor(Math.random() * 3)]
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <h1>Bottone dinamico</h1>
         <ButtonComponent text={scelto} />
+        <ButtonComponentDynamic />
         <h1>Immagine dinamica</h1>
         <ImageComponent src="https://placedog.net/300" alt="Doggo" css="App-logo" />
       </header>
---
+++ day1/src/components/ButtonComponentDynamic.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+class ButtonComponentDynamic extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { nome: "Inserisci" };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ nome: e.target.value });
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    placeholder="Scrivi qualcosa"
+                    onChange={this.handleChange}
+                    value={this.state.value}
+                />
+                <button className="button d-block">{this.state.nome}</button>
+            </div>
+        );
+    }
+}
+
+export default ButtonComponentDynamic
