70e8cb163140d7b43b61f2c2f00c25c544a619eb
Carmy93 <carmygargiulo45@gmail.com>
2024-08-21 15:08:02 +0200

    up

--- day2/src/components/SingleBook.jsx
+++ day2/src/components/SingleBook.jsx
@@ -6,16 +6,15 @@ class SingleBook extends Component {
         selected: false
     }
     handleClick = (e) => {
-        this.state.selected === false ? this.setState({ selected: true }) : this.setState({ selected: false })
-
-    }
-    handleStatus = (e) => {
-        e.stopPropagation()
-        if (this.state.selected) {
-            e.target.parentNode.style.boxShadow = '1px 1px 1px 1px lightgray'
-        } else {
+        /* this.state.selected === false ? this.setState({ selected: true }) : this.setState({ selected: false }) */
+        const sel = this.state.selected
+        this.setState({selected: !sel})
+        if (!sel) {
             e.target.parentNode.style.boxShadow = '8px 8px 8px 8px lightgray'
+        } else {
+            e.target.parentNode.style.boxShadow = '1px 1px 1px 1px lightgray'
         }
+
     }
 
     render() {
@@ -23,7 +22,7 @@ class SingleBook extends Component {
             <Container className='mb-5'>
                 <Row className='justify-content-center'>
                     <Col xs={12} md={6} lg={4}>
-                        <Card onClick={(e) => this.handleStatus(e)} className='h-100'>
+                        <Card className='h-100'>
                             <Card.Img variant="top" src={this.props.libro.img} onClick={(e) => this.handleClick(e)} />
                             <Card.Body className='d-flex flex-column justify-content-between'>
                                 <Card.Title className='fs-1'>{this.props.libro.title}</Card.Title>
