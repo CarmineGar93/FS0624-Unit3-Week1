b4ab428773a9d47419ad98e54ee799e41fe53e48
Carmy93 <carmygargiulo45@gmail.com>
2024-08-21 14:19:28 +0200

    up

--- day2/src/components/SingleBook.jsx
+++ day2/src/components/SingleBook.jsx
@@ -2,13 +2,13 @@ import { Col, Container, Row, Button, Card } from 'react-bootstrap'
 
 function SingleBook(props) {
     return (
-        <Container>
+        <Container className='mb-5'>
             <Row className='justify-content-center'>
                 <Col xs={12} md={6} lg={4}>
                     <Card className='h-100'>
                         <Card.Img variant="top" src={props.libro.img} />
                         <Card.Body className='d-flex flex-column justify-content-between'>
-                            <Card.Title className='fs-5'>{props.libro.title}</Card.Title>
+                            <Card.Title className='fs-1'>{props.libro.title}</Card.Title>
                             <Button variant="primary">$ {props.libro.price}</Button>
                         </Card.Body>
                     </Card>
