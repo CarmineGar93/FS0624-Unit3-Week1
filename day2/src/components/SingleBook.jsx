fe0fbd74c0b852bbf661b9d41efd703090358a1b
Carmy93 <carmygargiulo45@gmail.com>
2024-08-21 16:53:54 +0200

    migliorie stilistiche

--- day2/src/components/SingleBook.jsx
+++ day2/src/components/SingleBook.jsx
@@ -9,21 +9,15 @@ class SingleBook extends Component {
         /* this.state.selected === false ? this.setState({ selected: true }) : this.setState({ selected: false }) */
         const sel = this.state.selected
         this.setState({selected: !sel})
-/*         e.target.parentNode.classList.toggle('shadow') */
-        if (!sel) {
-            e.target.parentNode.style.boxShadow = '8px 8px 8px 8px lightgray'
-        } else {
-            e.target.parentNode.style.boxShadow = '0px 0px 0px 0px lightgray'
-        }
-
     }
 
     render() {
+        const cardClicked = this.state.selected ? 'shadowing' : ''
         return (
             <Container className='mb-5'>
                 <Row className='justify-content-center'>
                     <Col xs={12} md={6} lg={4}>
-                        <Card className='h-100'>
+                        <Card className={cardClicked}>
                             <Card.Img variant="top" src={this.props.libro.img} onClick={(e) => this.handleClick(e)} />
                             <Card.Body className='d-flex flex-column justify-content-between'>
                                 <Card.Title className='fs-1'>{this.props.libro.title}</Card.Title>
