8dfed68d4682ca63a2ea261abb3f9e7a1459f762
Carmy93 <carmygargiulo45@gmail.com>
2024-08-22 15:56:29 +0200

    bottone delete

--- day2/src/components/CommentsList.jsx
+++ day2/src/components/CommentsList.jsx
@@ -1,6 +1,33 @@
 import { Component } from "react";
-import { ListGroup } from 'react-bootstrap'
+import { Button, ListGroup } from 'react-bootstrap'
 class CommentsList extends Component {
+    state = {
+        reloaded: false
+    }
+    handleClick = async (e, id) => {
+        e.preventDefault()
+        const sure = window.confirm('Sei sicuro?')
+        if (sure) {
+            try {
+                const URL = 'https://striveschool-api.herokuapp.com/api/comments/'
+                const asin = id
+                const response = await fetch(URL + asin, {
+                    method: 'DELETE',
+                headers: {
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MmM3NDI4YWI5NjAwMTU2NjRmMGUiLCJpYXQiOjE3MjQzMjkwNzYsImV4cCI6MTcyNTUzODY3Nn0.Si6MDHOC4QOt-RT6rUZF7zUYk6RqmKdoPXyQKANzAYw"
+                }
+                })
+                if (response.ok) {
+                    alert('Recensione cancellata correttamente')
+                    this.setState({reloaded: true})
+                } else {
+                    throw new Error('Errore nella cancellazione del commento')
+                }
+            } catch (err) {
+                alert(err)
+            }
+        }
+    }
     render() {
         return (
             <ListGroup>
@@ -9,7 +36,11 @@ class CommentsList extends Component {
                         <ListGroup.Item>Nessun commento per questo libro</ListGroup.Item>
                     ) : (
                         this.props.array.map((libro) => {
-                            return <ListGroup.Item key={libro._id}>{libro.rate} | {libro.comment}</ListGroup.Item>
+                            return (
+                            <ListGroup.Item key={libro._id} className="d-flex align-items-center">
+                                <span>{libro.rate} | {libro.comment} </span>
+                                <Button onClick={(e) => this.handleClick(e, libro._id)} variant="danger" className="ms-auto">X</Button>
+                            </ListGroup.Item>)
                         })
                     )
                 }
