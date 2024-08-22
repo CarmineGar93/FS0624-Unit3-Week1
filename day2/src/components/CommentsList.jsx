375c1cbb63d197ff67d70f5bb491fd113afc1ec8
Carmy93 <carmygargiulo45@gmail.com>
2024-08-22 18:02:24 +0200

    stelline

--- day2/src/components/CommentsList.jsx
+++ day2/src/components/CommentsList.jsx
@@ -13,13 +13,13 @@ class CommentsList extends Component {
                 const asin = id
                 const response = await fetch(URL + asin, {
                     method: 'DELETE',
-                headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MmM3NDI4YWI5NjAwMTU2NjRmMGUiLCJpYXQiOjE3MjQzMjkwNzYsImV4cCI6MTcyNTUzODY3Nn0.Si6MDHOC4QOt-RT6rUZF7zUYk6RqmKdoPXyQKANzAYw"
-                }
+                    headers: {
+                        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MmM3NDI4YWI5NjAwMTU2NjRmMGUiLCJpYXQiOjE3MjQzMjkwNzYsImV4cCI6MTcyNTUzODY3Nn0.Si6MDHOC4QOt-RT6rUZF7zUYk6RqmKdoPXyQKANzAYw"
+                    }
                 })
                 if (response.ok) {
                     alert('Recensione cancellata correttamente')
-                    this.setState({reloaded: true})
+                    this.setState({ reloaded: true })
                 } else {
                     throw new Error('Errore nella cancellazione del commento')
                 }
@@ -36,15 +36,32 @@ class CommentsList extends Component {
                         <ListGroup.Item>Nessun commento per questo libro</ListGroup.Item>
                     ) : (
                         this.props.array.map((libro) => {
+                            const star = []
+                            for (let i = 0; i < libro.rate; i++) {
+                                star.push(<span className="d-flex align-items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="yellow" className="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                  </svg></span>)
+                            }
                             return (
-                            <ListGroup.Item key={libro._id} className="d-flex align-items-center">
-                                <span>{libro.rate} | {libro.comment} </span>
-                                <Button onClick={(e) => this.handleClick(e, libro._id)} variant="danger" className="ms-auto">X</Button>
-                            </ListGroup.Item>)
+                                <ListGroup.Item key={libro._id} className="d-flex align-items-center">
+                                    <div className="ms-1">
+                                        <div className="d-flex align-items-center">
+                                            <i>Rate: &nbsp;</i>
+                                            {
+                                                star.map((stars) => {
+                                                    return stars
+                                                })
+                                            }
+                                        </div>
+
+                                        <span><i>Commento:</i> {libro.comment} </span>
+                                    </div>
+                                    <Button onClick={(e) => this.handleClick(e, libro._id)} variant="danger" className="ms-auto">X</Button>
+                                </ListGroup.Item>)
                         })
                     )
                 }
-                
+
             </ListGroup>
         )
     }
